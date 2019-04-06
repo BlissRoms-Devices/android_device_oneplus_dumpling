0a7e8ec0d15bc6976beb421e69f4d892027d01c7
Henrique Pereira <pereirah2001@gmail.com>
2019-04-06 12:26:45 -0300

    dumpling: Restore Keylayout
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- device.mk
+++ device.mk
@@ -40,5 +40,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gf_input.kl:system/usr/keylayout/gf_input.kl
+
 # Inherit from oneplus msm8998-common
 $(call inherit-product, device/oneplus/msm8998-common/common.mk)
