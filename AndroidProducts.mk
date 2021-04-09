5b0d64299b3098d82706c5b02f4a4b5238d56736
palaych <palaychm@yandex.ru>
2021-04-09 19:27:55 +0000

    dumpling: Adapt for ArrowOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/arrow_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    arrow_dumpling-user \
+    arrow_dumpling-userdebug \
+    arrow_dumpling-eng
--- arrow_dumpling.mk
+++ arrow_dumpling.mk
@@ -21,10 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dumpling device
 $(call inherit-product, device/oneplus/dumpling/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := lineage_dumpling
+DEVICE_MAINTAINER := Bauuuuu
+
+PRODUCT_NAME := arrow_dumpling
 PRODUCT_DEVICE := dumpling
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
